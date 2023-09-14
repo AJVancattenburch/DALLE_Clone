394d3c9ba8177783beb4fbfe6b29ef56c8e0eab1
Anthony Vancattenburch <wanderwithme1988@gmail.com>
2023-09-14 14:12:37 -0600

    test

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -56,7 +56,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch('https://dalle-clone-8hpg.onrender.com/api/v1/post', {
+        const response = await fetch('https://localhost:8080/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.get("/", async (req, res) => {
 });
 
 const startServer = async () => {
-  const port = process.env.PORT || 5000;
+  const port = process.env.PORT || process.env.DEV_PORT;
   try {
     connectDB(process.env.MONGODB_URL);
     app.listen(port, () =>
