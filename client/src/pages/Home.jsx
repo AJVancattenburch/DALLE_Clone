7202f9721e8b4af00fbe710a96a44b36c5a1cae1
Anthony Vancattenburch <wanderwithme1988@gmail.com>
2023-09-13 19:59:40 -0600

    update fetch endpoint

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
       setLoading(true);
 
       try {
-        const res = await fetch('https://dalleclone-kmss.onrender.com/api/v1/post', {
+        const res = await fetch('https://dalle-clone-8hpg.onrender.com/api/v1/post', {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
