37bb5f34be174ca786278478ea590832c047661d
Anthony Vancattenburch <wanderwithme1988@gmail.com>
2023-09-13 19:57:28 -0600

    fixes

--- server/index.js
+++ server/index.js
@@ -1,35 +1,33 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import cors from 'cors';
-
-import connectDB from './mongoDB/connect.js';
-import postRoutes from './routes/postRoutes.js';
-import dalleRoutes from './routes/dalleRoutes.js';
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./mongodb/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import dalleRoutes from "./routes/dalleRoutes.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: "50mb" }));
 
 app.use('/api/v1/post', postRoutes);
 app.use('/api/v1/dalle', dalleRoutes);
 
-app.get('/', async (req, res) => {
-  res.send('Hello from DALL-E!');
-})
+app.get("/", async (req, res) => {
+  res.send("Hello from DALL-E");
+});
 
 const startServer = async () => {
-
+  const port = process.env.PORT || 5000;
   try {
     connectDB(process.env.MONGODB_URL);
-    app.listen(8080, () => 
-      console.log('🎉🥂 Server is listening on port http://localhost:8080 🥂🎉')
+    app.listen(port, () =>
+      console.log(`Server running on port : ${port} 🔥`)
     );
   } catch (error) {
     console.log(error);
   }
-
-}
+};
 
 startServer();
