96fd5a9033fc11a55686606cf0d2b75e3155c923
Anthony Vancattenburch <wanderwithme1988@gmail.com>
2023-09-15 11:31:02 -0600

    fix host

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
       setLoading(true);
 
       try {
-        const res = await fetch('https://dalle-clone-8hpg.onrender.com/api/v1/post', {
+        const res = await fetch('http://localhost:8080/api/v1/post', {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
